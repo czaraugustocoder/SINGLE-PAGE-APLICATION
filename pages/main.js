1993de7ba92fc2e21e93a0760d6e07616e21d9d3
czaraugustocoder <108474749+czaraugustocoder@users.noreply.github.com>
2023-02-23 16:09:35 -0400

    criação do tamplate HOME

---
+++ pages/main.js
@@ -0,0 +1,10 @@
+export default main = function() {
+    const container = document.createElement('div');
+
+    const template = `
+    <h2>Cesar Augusto Andrade Ferreira</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora magni nesciunt quibusdam velit quo vel, ratione recusandae, quae neque incidunt reprehenderit nam totam eius quia fugiat laudantium fugit debitis porro?</p>
+    `;
+
+    container.innerHTML = template;
+}
