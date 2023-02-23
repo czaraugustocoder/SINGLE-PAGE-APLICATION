cdee844c9a976389196c9f77021fb57f177159f1
czaraugustocoder <108474749+czaraugustocoder@users.noreply.github.com>
2023-02-23 18:15:41 -0400

    adição de hashchange listener in index.js e mudança de urls

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const root = document.querySelector("#root");
 
+function init(){
+    window.addEventListener("hashchange", function(){
+        console.log(window.location.hash)
+    })
+}
+
 window.addEventListener("load", () => {
     root.appendChild(cesar());
+    init();
 })
