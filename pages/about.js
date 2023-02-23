cdd08b9496c770ceb03ee9155a026c76fe0703d4
czaraugustocoder <108474749+czaraugustocoder@users.noreply.github.com>
2023-02-23 18:37:40 -0400

    atualização de texto

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,10 @@ function  about() {
     const template = `
     <h2>ABOUT</h2>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora magni nesciunt quibusdam velit quo vel, ratione recusandae, quae neque incidunt reprehenderit nam totam eius quia fugiat laudantium fugit debitis porro?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora magni nesciunt quibusdam velit quo vel, ratione recusandae, quae neque incidunt reprehenderit nam totam eius quia fugiat laudantium fugit debitis porro?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora magni nesciunt quibusdam velit quo vel, ratione recusandae, quae neque incidunt reprehenderit nam totam eius quia fugiat laudantium fugit debitis porro?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora magni nesciunt quibusdam velit quo vel, ratione recusandae, quae neque incidunt reprehenderit nam totam eius quia fugiat laudantium fugit debitis porro?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora magni nesciunt quibusdam velit quo vel, ratione recusandae, quae neque incidunt reprehenderit nam totam eius quia fugiat laudantium fugit debitis porro?</p>
     `;
 
     container.innerHTML = template;
--- pages/contato.js
+++ pages/contato.js
@@ -4,6 +4,8 @@ function  contato() {
     const template = `
     <h2>CONTATO</h2>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora magni nesciunt quibusdam velit quo vel, ratione recusandae, quae neque incidunt reprehenderit nam totam eius quia fugiat laudantium fugit debitis porro?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora magni nesciunt quibusdam velit quo vel, ratione recusandae, quae neque incidunt reprehenderit nam totam eius quia fugiat laudantium fugit debitis porro?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora magni nesciunt quibusdam velit quo vel, ratione recusandae, quae neque incidunt reprehenderit nam totam eius quia fugiat laudantium fugit debitis porro?</p>
     `;
 
     container.innerHTML = template;
