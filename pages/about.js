67456ad52f066601aa4ec552803dceb1ee7c677e
czaraugustocoder <108474749+czaraugustocoder@users.noreply.github.com>
2023-02-23 18:29:07 -0400

    criação e referenciação em index.html dos templates(javascript functions) about.js e contato.js

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+function  about() {
+    const container = document.createElement('div');
+
+    const template = `
+    <h2>ABOUT</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora magni nesciunt quibusdam velit quo vel, ratione recusandae, quae neque incidunt reprehenderit nam totam eius quia fugiat laudantium fugit debitis porro?</p>
+    `;
+
+    container.innerHTML = template;
+
+    return container;
+}
---
+++ pages/contato.js
@@ -0,0 +1,12 @@
+function  contato() {
+    const container = document.createElement('div');
+
+    const template = `
+    <h2>CONTATO</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora magni nesciunt quibusdam velit quo vel, ratione recusandae, quae neque incidunt reprehenderit nam totam eius quia fugiat laudantium fugit debitis porro?</p>
+    `;
+
+    container.innerHTML = template;
+
+    return container;
+}
