c7609dcdd69c00e9849e69019f60972d87821bb9
czaraugustocoder <108474749+czaraugustocoder@users.noreply.github.com>
2023-02-23 18:20:40 -0400

    adição de um switch case para mudança de páginas in inde.js

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const root = document.querySelector("#root");
 function init(){
     window.addEventListener("hashchange", function(){
         console.log(window.location.hash)
+        switch(window.location.hash){
+            case "#cesar":
+            root.appendChild(cesar());
+            break;
+            case "#about":
+            root.appendChild(cesar());
+            break;
+            case "#contatos":
+            root.appendChild(cesar());
+            break;
+        }
     })
 }
 
